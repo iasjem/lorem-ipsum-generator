2b2d9f5a2acb039def1fd31f00d5bcdda1fd42a8
iasjem <jemimah.beryl.sai@gmail.com>
2019-08-21 21:35:13 +0800

    Modify script.js - initial setup
    - Name as variables for each JQUERY components:
    --- fieldOutputCount
    --- selectorOutputType
    --- buttonGenerateOutput
    --- buttonCopyOutput
    --- divOutputDisplay
    
    - Create object literals for constant values:
    --- SET_OF_WORDS
    --- FIRST_SENTENCE
    
    - Create functions:
    --- getCountOfWords
    --- getSetOfWords
    --- setWords
    --- setSentences
    --- setParagraphs
    
    - Setup JQUERY Components' events:
    --- document (for initialization)
    --- buttonGenerateOutput
    --- buttonCopyOutput

---
+++ script.js
@@ -0,0 +1,65 @@
+const fieldOutputCount = $('#input_count_of_output');
+const selectorOutputType = $('#select_type_of_output');
+const buttonGenerateOutput = $('#button_generate_output');
+const buttonCopyOutput = $('#button_copy_output');
+const divOutputDisplay = $('#div_output_display');
+
+
+/** OBJECT LITERALS */
+
+const SET_OF_WORDS = [
+    "vel", "cursus", "metus", "aliquam", "eleifend", "mi", "in", "id", "neque", "nulla", "posuere",
+    "ullamcorper", "dignissim", "cras", "tincidunt", "lobortis", "feugiat", "vivamus", "mattis", "nunc",
+    "sed", "blandit", "libero", "massa", "neque", "a", "diam", "elit", "faucibus", "ornare", "auctor",
+    "ac", "quisque", "rutrum", "gravida", "hendrerit", "est", "nibh", "scelerisque", "fermentum", "dui",
+    "ante", "adipiscing", "consectetur", "dolor", "sit", "ipsum", "lorem", "sollicitudin", "tempor", "turpis",
+    "labore", "ut", "eiusmod", "do", "orci", "pharetra", "eget", "tristique", "porttitor", "quam", "vitae", "torro",
+    "commodo", "mauris", "morbi", "placerat", "fusce", "eu", "quis", "eros", "donec", "odio", "venenatis", "lectus",
+    "augue", "purus", "scelerisque", "hac", "enim", "tellus", "platea", "habitasse", "arcu", "dictumst", "ultricies"
+];
+
+const FIRST_SENTENCE = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+
+/** FUNCTIONS */
+
+const getCountOfWords = () => {
+    return 0;
+}
+
+const getSetOfWords = () => {
+    return SET_OF_WORDS[0];
+};
+
+const setWords = (count) => {
+    let words = '';
+
+    return words;
+};
+
+const setSentences = (count) => {
+    let sentences = '';
+
+    return sentences;
+};
+
+const setParagraphs = (count) => {
+    let paragraphs = '';
+
+    return paragraphs;
+};
+
+
+/** JQUERY COMPONENTS */
+
+$(document).ready(function () {
+
+});
+
+buttonGenerateOutput.click(function () {
+
+});
+
+buttonCopyOutput.click(function () {
+
+});
